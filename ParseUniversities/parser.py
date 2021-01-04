d1b7295114f14594d3b59561b04ed46861c55358
Melnik Nikolai <c.7843543@gmail.com>
2021-01-04 23:25:09 +0300

    moving from sharp to python

--- ParseUniversities/parser.py
+++ ParseUniversities/parser.py
@@ -7,8 +7,8 @@ page.encoding = "windows-1251"
 soup  = bs(page.text, "lxml")
 
 Federal_districts = soup.findAll('p', class_ = 'MsoListParagraph')
-Federal_districts_links = []
-for dist_ in Federal_districts:
-    d = bs(dist_, "lxml")
-    print(d.findAll('href'))
-#print(Federal_districts)
+#Federal_districts_links = []
+#for dist_ in Federal_districts:
+ #   d = bs(dist_, "lxml")
+  #  print(d.findAll('href'))
+print(Federal_districts)
