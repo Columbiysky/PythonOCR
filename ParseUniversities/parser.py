aacc243115a86fb2155620b552b82db8bc3c86ce
Melnik Nikolai <c.7843543@gmail.com>
2021-01-27 00:32:05 +0300

    reworked method for getting links

--- ParseUniversities/parser.py
+++ ParseUniversities/parser.py
@@ -3,6 +3,7 @@ import requests
 import os
 import platform
 import subprocess
+import urllib.request as downloader
 
 base_url  = 'http://indicators.miccedu.ru/monitoring/2019/'
 
@@ -35,7 +36,7 @@ def GetUniversitiesPages(federalDistrictLinks):
     return uni_pages
 
 def GetUniversitiesLinks(universitiesPages):
-    universitiesLinks = []
+    universitiesLinks = {}
     private = False
     for link in universitiesPages:    
         page = requests.get(link)
@@ -44,21 +45,53 @@ def GetUniversitiesLinks(universitiesPages):
         count = 0
         for table in soup.find_all('table', id = 'info'):
             private = False
-            for td in table("td"):
+            uni_branch = False
+            name = ""
+            for td in table("td"):                
                 count = count + 1
+                if(count == 2):
+                    name = td.get_text()
+                
+                if(name.find('филиал') > -1 or name.find('Филиал') > -1):
+                    uni_branch = True
+
                 if(count == 6 and td.get_text() == 'Частные образовательные организации'):
                     private = True
 
-                if(count == 8 and private == False):
-                    universitiesLinks.append(td.get_text())
+                if(count == 8 and private == False and uni_branch == False):
+                    link = td.get_text()
+                    # Normalize links Example: uni-dubna.ru
+                    if (link.find('https://') != -1):
+                        link = link.replace('https://','')
+                    if(link.find('http://') != -1):
+                        link = link.replace('http://', '')
+                    if(link.find('www.') != -1):
+                        link = link.replace('www.','')
+                    if(link.find('www ') > -1):
+                        link = link.replace('www ','')
+                    if(link.find('mtuci.ru') > -1):
+                        link = 'mtuci.ru'
+                    if(link.find('academy.andriaka.ru') > -1):
+                        link = 'academy.andriaka.ru'
+                    if(link.find('sgap.ru') > -1): # cuz .ru domen belong Arkhangelsk branch of CГЮА
+                        link = 'сгюа.рф'
+                    if(link.find('bigpi.biysk.ru/wwwsite') > -1):
+                        link = 'bigpi.biysk.ru'
+                    if(link.find('astracons.ru') > -1):
+                        link = 'astracons.ru'                    
+                    if(link.find('http:\\')> -1):
+                        link = link.replace('http:\\','')
+                    if(link.find('/') > -1):
+                        link = link.replace('/', '')
+                    universitiesLinks[name]=link
                     break
 
     return universitiesLinks
 
 def PrintUniversitiesLinksInFile(universitiesLinks):
-    f = open('unisNoPrivate.txt', 'w')
-    for i in universitiesLinks:
-        f.write(i + '\n')
+    f = open('unisLinks.txt', 'w', encoding='windows-1251')
+    for k,v in universitiesLinks.items():
+        f.write(v + '\n')
     f.close()
 
 def ping(host):
@@ -76,19 +109,22 @@ def ping(host):
 
     return response
 
-def check(): pass
+def DownloadTest():
+    link  = 'https://xn--d1aixi.xn--p1ai/sveden/document/%D0%9F%D1%80%20%E2%84%96%20403-%D0%A2%20%D0%BE%D1%82%2028.08.20.%20%D0%9E%20%D1%81%D1%82%D0%BE%D0%B8%D0%BC%D0%BE%D1%81%D1%82%D0%B8%201%20%D0%BA%D1%83%D1%80%D1%81%D0%B0%20%D0%92%D0%9E%20%D0%B8%20%D0%A1%D0%9F%D0%9E%20%D0%BE%D1%87%D0%BD%D0%BE%D0%B9%20%D1%84%D0%BE%D1%80%D0%BC%D1%8B%20%D0%BE%D0%B1%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D1%8F.pdf'
+    downloader.urlretrieve(link, './pizdec.pdf')
 
 
 def main():
     # res = ping('asdfasdf.asdf')
     # print(res)
+
     if os.path.exists('test.txt'):
         f = open('test.txt','r')
         r = open('res.txt','w')
         for line in f:
             res = ping(line)
             if (res != 0):
-                r.write(line + '\n')
+                r.write(line)
         r.close()
         f.close()
 
@@ -97,6 +133,7 @@ def main():
         universitiesPages = GetUniversitiesPages(federalDistrictLinks)
         universitiesLinks = GetUniversitiesLinks(universitiesPages)
         PrintUniversitiesLinksInFile(universitiesLinks)
+    # DownloadTest()
 
 if __name__ == '__main__' :
     main()
